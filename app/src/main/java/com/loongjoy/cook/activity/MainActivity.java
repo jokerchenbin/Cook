5d439b6c8a5247be3eb66bc481fa2dee5b512e16
bin_chen <bin_chen@loongjoy.com>
2016-03-08 11:41:00 +0800

    首页

--- app/src/main/java/com/loongjoy/cook/activity/MainActivity.java
+++ app/src/main/java/com/loongjoy/cook/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.loongjoy.cook.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.loongjoy.cook.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<Fragment> fragmentlist = new ArrayList<Fragment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
